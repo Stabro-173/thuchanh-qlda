01be34677e525563ff95ee5789afc7afd26e26de
hourin <phucjaki@gmail.com>
2026-03-29 16:52:35 +0700

    Added: comments to make functions' purposes clearer

--- vector.hh
+++ vector.hh
@@ -16,21 +16,30 @@
 class FloatVector {
 public:
         // Thanh vien 1
-        void push(float n);
-        float pop();
+        FloatVector();  // Tao moi array _data bang new hoac malloc().
+        void push(float n); // Them phan tu n vao cuoi vector.
+        float pop();    // Xoa phan tu cuoi cung (phan tu duoc push
+                        // gan day nhat) roi tra ve gia tri phan tu do.
 
         // Thanh vien 2
         float &operator[](size_t idx);
         float operator[](size_t idx) const;
-        FloatVector operator=(const FloatVector&) const;
+
+        // Thanh vien 2 luu y: ham nay thuc hien deep copy. Thay vi
+        // chi gan _data cua FloatVector nay vao _data cua FloatVector
+        // kia thi phai copy tung phan tu cua FloatVector kia, de tranh
+        // bug double free.
+        FloatVector operator=(const FloatVector&); // Gan vector nay
+                                                   // vao vector kia.
 
         // Thanh vien 3
-        bool operator==(const FloatVector&) const;
-        FloatVector operator*(float n) const;
-        FloatVector operator+(float n) const;
+        bool operator==(const FloatVector&) const; // So sanh 2 vector.
+        FloatVector operator*(float n) const;   // Nhan moi phan tu voi n.
+        FloatVector operator+(float n) const;   // Cong moi phan tu voi n.
 
         // Thanh vien 4 (neu co)
-        ~FloatVector();
+        ~FloatVector(); // Don dep bo nho. Dung delete hoac free() tuy vao
+                        // quyet dinh cua thanh vien 1 trong FloatVector().
         size_t get_size() const;
         size_t get_capacity() const;
 
@@ -49,7 +58,9 @@ public:
                 return *this * (1.0f/n);
         }
 private:
-        // Thanh vien 1
+        // Thanh vien 1.
+        // Cap lai bo nho moi cho pointer _data. Co the cap truoc
+        // bo nho cho nhieu phan tu de tang toc do xu li.
         void _realloc(size_t new_size);
 
         float *_data;
